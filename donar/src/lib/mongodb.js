4a339126fc1c62aa1479dda86786915bd670caf1
webenor123 <developer@webenor.com>
2025-05-21 10:40:57 +0530

    username password file defined in dbconnect file
    username password file defined in dbconnect file

--- donar/src/lib/mongodb.js
+++ donar/src/lib/mongodb.js
@@ -1,10 +1,12 @@
 // /src/lib/mongodb.js
 import { MongoClient } from 'mongodb';
 
-// const username = encodeURIComponent(process.env.MONGODB_USERNAME);
-// const password = encodeURIComponent(process.env.MONGODB_PASSWORD); 
+ 
 
-const MONGODB_URI_PAYMENTS = `mongodb+srv://arvind_kumar:0r8Yx9aInHCKh566@cluster0.7beqiva.mongodb.net/payments?retryWrites=true&w=majority&appName=Cluster0`;
+const username = "arvind_kumar"
+const password = "0r8Yx9aInHCKh566"
+
+const MONGODB_URI_PAYMENTS = `mongodb+srv://${username}:${password}@cluster0.7beqiva.mongodb.net/payments?retryWrites=true&w=majority&appName=Cluster0`;
 
 const options = {
   tls: true, // Only keep supported options
